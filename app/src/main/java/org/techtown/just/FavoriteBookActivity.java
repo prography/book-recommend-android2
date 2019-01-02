5da92f90b0339df0e789536bfcdf27058158538e
daheeahn <deg9810@likelion.org>
2019-01-02 20:26:57 +0900

    activity 만들었음

---
+++ app/src/main/java/org/techtown/just/FavoriteBookActivity.java
@@ -0,0 +1,13 @@
+package org.techtown.just;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoriteBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_book);
+    }
+}
