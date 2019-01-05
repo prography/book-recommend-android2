bd712554eed93db91ca2436337a4e51aba4bb8cf
daheeahn <deg9810@likelion.org>
2019-01-05 16:46:37 +0900

    activity 연결 - login되어있으면 mypage로, 아니면 login으로 / sharedPreferences

--- app/src/main/java/org/techtown/just/MainActivity.java
+++ app/src/main/java/org/techtown/just/MainActivity.java
@@ -1,13 +1,81 @@
 package org.techtown.just;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    String sfName = "myFile";
+    @BindView(R.id.btn_my)
+    Button btnMy;
+    @BindView(R.id.text)
+    TextView text;
+    @BindView(R.id.btn_anything)
+    Button btnAnything;
+    @BindView(R.id.editText1)
+    EditText editText1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // 지난번 저장해놨던 사용자 입력값을 꺼내서 보여주기
+        SharedPreferences sf = getSharedPreferences(sfName, 0);
+        String str = sf.getString("name", "a"); // 키값으로 꺼냄
+        editText1.setText(str); // EditText에 반영함
+
+        //btnMy
+        btnMy.setOnClickListener(this);
+    } // end of onCreate
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Activity 가 종료되기 전에 저장한다
+        // SharedPreferences 에 설정값(특별히 기억해야할 사용자 값)을 저장하기
+        SharedPreferences sf = getSharedPreferences(sfName, 0);
+        SharedPreferences.Editor editor = sf.edit();//저장하려면 editor가 필요
+        String str = editText1.getText().toString(); // 사용자가 입력한 값
+        editor.putString("name", str); // 입력
+        editor.commit(); // 파일에 최종 반영함
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.btn_my:
+                // SharedPreferences 에 설정값(특별히 기억해야할 사용자 값)을 저장하기
+                SharedPreferences sf = getSharedPreferences(sfName, 0);
+                SharedPreferences.Editor editor = sf.edit();//저장하려면 editor가 필요
+
+                int i = sf.getInt("my", 0);
+                if (i == 0)
+                   intent = new Intent(this, LoginActivity.class);
+                else
+                    intent = new Intent(this, MyPageActivity.class);
+
+                startActivity(intent);
+
+                editor.putInt("my", 1); // 입력
+                editor.commit(); // 파일에 최종 반영함
+                break;
+        }
     }
 }
+
+
+
--- app/src/main/java/org/techtown/just/MyPageActivity.java
+++ app/src/main/java/org/techtown/just/MyPageActivity.java
@@ -1,13 +1,38 @@
 package org.techtown.just;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MyPageActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MyPageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.btn_main)
+    Button btn_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+
+        ButterKnife.bind(this);
+
+        btn_main.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch(view.getId()) {
+            case R.id.btn_main:
+                intent = new Intent(this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                break;
+        }
     }
 }
