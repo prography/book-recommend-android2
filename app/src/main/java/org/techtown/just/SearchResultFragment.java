50d6599a5f8adde8a4cb356d48c56c61cd14de6b
daheeahn <deg9810@likelion.org>
2019-01-08 19:22:39 +0900

    search activity

---
+++ app/src/main/java/org/techtown/just/SearchResultFragment.java
@@ -0,0 +1,36 @@
+package org.techtown.just;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class SearchFragment extends Fragment {
+
+    MainActivity activity;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        activity = (MainActivity) getActivity(); //?
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        activity = null; //??
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_search, container, false);
+
+
+        return rootView;
+    }
+}
