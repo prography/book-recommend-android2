a0d9eb2bc45e7ef569a3314600237fce78a82a37
daheeahn <deg9810@likelion.org>
2019-01-19 16:38:24 +0900

    book detail

---
+++ app/src/main/java/org/techtown/just/PostDetailActivity.java
@@ -0,0 +1,26 @@
+package org.techtown.just;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PostDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.str)
+    TextView str;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_detail);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        Post post = (Post) intent.getSerializableExtra("post");
+        str.setText(post.toString());
+    }
+}
--- app/src/main/java/org/techtown/just/PostsActivity.java
+++ app/src/main/java/org/techtown/just/PostsActivity.java
@@ -1,5 +1,6 @@
 package org.techtown.just;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,7 +13,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class PostsActivity extends AppCompatActivity {
+public class PostsActivity extends AppCompatActivity implements OnClickListener {
 
     MyRecyclerViewAdapter myRecyclerViewAdapter;
 
@@ -27,20 +28,28 @@ public class PostsActivity extends AppCompatActivity {
         myRecyclerViewAdapter = new MyRecyclerViewAdapter();
         view.setLayoutManager(layoutManager);
         view.setAdapter(myRecyclerViewAdapter);
+        myRecyclerViewAdapter.setClickListener(this);
 
         //retrofit
-        Call<List<Comment>> comment = NetworkManager.getApiService().getComments(1);
-        comment.enqueue(new Callback<List<Comment>>() {
+        Call<List<Post>> post = NetworkManager.getApiService().getPostList();
+        post.enqueue(new Callback<List<Post>>() {
             @Override
-            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
-                List<Comment> comments = response.body();
-                myRecyclerViewAdapter.addComments(comments);
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                List<Post> posts = response.body();
+                myRecyclerViewAdapter.addComments(posts);
             }
 
             @Override
-            public void onFailure(Call<List<Comment>> call, Throwable t) {
+            public void onFailure(Call<List<Post>> call, Throwable t) {
                 Toast.makeText(PostsActivity.this, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onPostClick(Post post) {
+        Intent intent = new Intent(PostsActivity.this, PostDetailActivity.class);
+        intent.putExtra("post", post);
+        startActivity(intent);
     }
 }
